8b35f40f2d31a39484eeb6d32b18b54d1f6d02bf
Rholais <lhch95@163.com>
2015-11-21 17:57:56 +0800

    Create `ExbServlet`

---
+++ src/mid/ExbServlet.java
@@ -0,0 +1,22 @@
+package mid;
+
+import java.io.IOException;
+
+/**
+ * Created by lhch on 2015/11/21 0021.
+ */
+public class ExbServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(
+            javax.servlet.http.HttpServletRequest request,
+            javax.servlet.http.HttpServletResponse response)
+            throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(
+            javax.servlet.http.HttpServletRequest request,
+            javax.servlet.http.HttpServletResponse response)
+            throws javax.servlet.ServletException, IOException {
+
+    }
+}
