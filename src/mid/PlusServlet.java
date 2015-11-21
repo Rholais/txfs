4f47c44fa115fb8e101967142c07e4957dc2d17f
Rholais <lhch95@163.com>
2015-11-21 21:28:01 +0800

    Add `doGet` method to `PlusServlet`

--- src/mid/PlusServlet.java
+++ src/mid/PlusServlet.java
@@ -1,45 +1,43 @@
 package mid;
 
+import bean.*;
+import com.google.gson.Gson;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-
-import bean.*;
-import com.google.gson.Gson;
 
 /**
  * Created by lhch on 2015/11/21 0021.
  */
-@WebServlet(name = "ExbServlet")
-public class ExbServlet extends HttpServlet {
+@WebServlet(name = "PlusServlet")
+public class PlusServlet extends HttpServlet {
 
-    private Exhibit exb;
-    private ArrayList<Message> msg_al;
+    private Boolean plus;
 
     protected void doPost(
             HttpServletRequest request,
             HttpServletResponse response)
             throws ServletException, IOException {
 
-     }
+    }
 
     protected void doGet(
-            javax.servlet.http.HttpServletRequest request,
-            javax.servlet.http.HttpServletResponse response)
-            throws javax.servlet.ServletException, IOException {
+            HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
         response.setContentType("application/json");
         request.setCharacterEncoding("utf-8");
         response.setCharacterEncoding("utf-8");
-        int exb_id = Integer.parseInt(request.getParameter("id"));
-        exb = new Exhibit(exb_id);
-        Message msg = new Message(0, exb_id, 0);
+        int msg_id = Integer.parseInt(request.getParameter("id"));
+        Message msg = new Message(msg_id, 0, 0);
         DB db = new DB();
-        exb = exb.selectExhibitById(db).get(0);
-        msg_al = msg.selectMessageByExb(db);
+        msg = msg.selectMessageById(db).get(0);
+        msg.setMsg_plus(msg.getMsg_plus() + 1);
+        plus = msg.updateMessage(db) != 0;
         Gson gson = new Gson();
         String json = gson.toJson(this);
         response.getWriter().print(json);
