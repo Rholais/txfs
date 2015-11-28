9927a8113738999536bf11a29e3728fe8079404e
Rholais Lii <rholais@gmail.com>
2015-11-28 16:57:54 +0800

    Fetch url

--- src/node/server.js
+++ src/node/server.js
@@ -1,13 +1,15 @@
 var http = require("http");
+var url = require("url");
 
 function start() {
 	function onRequest(request, response) {
-		console.log("Request received.");
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received.");
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write("Hello World");
 		response.end();
 	}
-
+	
 	http.createServer(onRequest).listen(8888);
 	console.log("Server has started.");
 }
