0e2c19ffa158936fe5ca79c471343a9d9a84eb31
Rholais Lii <rholais@gmail.com>
2015-11-29 11:07:05 +0800

    Send `response` to `route`

--- src/node/server.js
+++ src/node/server.js
@@ -5,12 +5,8 @@ function start(route, handle) {
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
-		
-		route(handle, pathname);
-		
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write("Hello World");
-		response.end();
+	
+	route(handle, pathname, response);
 	}
 	
 	http.createServer(onRequest).listen(8888);
