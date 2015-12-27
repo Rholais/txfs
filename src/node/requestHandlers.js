d4b48a5cac1000205cbf6ae3a30ba16484fe4fd9
Rholais Lii <rholais@gmail.com>
2015-12-28 00:41:23 +0800

    Input file

--- src/node/requestHandlers.js
+++ src/node/requestHandlers.js
@@ -10,9 +10,10 @@ function start(response, postData) {
     'content="text/html; charset=UTF-8" />'+
     '</head>'+
     '<body>'+
-    '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
-    '<input type="submit" value="Submit text" />'+
+    '<form action="/upload" enctype="multipart/form-data" '+
+    'method="post">'+
+    '<input type="file" name="upload">'+
+    '<input type="submit" value="Upload file" />'+
     '</form>'+
     '</body>'+
     '</html>';
@@ -23,30 +24,26 @@ function start(response, postData) {
 }
 
 function upload(response, postData) {
-	console.log("Request handler 'upload' was called.");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("You've sent the text: "+
-	querystring.parse(postData).text);
-	response.end();
+  console.log("Request handler 'upload' was called.");
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("You've sent the text: "+
+  querystring.parse(postData).text);
+  response.end();
 }
 
 function show(response, postData) {
-	console.log("Request handler 'show' was called.");
-	fs.readFile(
-		"/tmp/test.png", 
-		"binary", 
-		function(error, file) {
-			if(error) {
-				response.writeHead(500, {"Content-Type": "text/plain"});
-				response.write(error + "\n");
-				response.end();
-			} else {
-				response.writeHead(200, {"Content-Type": "image/png"});
-				response.write(file, "binary");
-				response.end();
-			}
-		}
-	);
+  console.log("Request handler 'show' was called.");
+  fs.readFile("/tmp/test.png", "binary", function(error, file) {
+    if(error) {
+      response.writeHead(500, {"Content-Type": "text/plain"});
+      response.write(error + "\n");
+      response.end();
+    } else {
+      response.writeHead(200, {"Content-Type": "image/png"});
+      response.write(file, "binary");
+      response.end();
+    }
+  });
 }
 
 exports.start = start;
